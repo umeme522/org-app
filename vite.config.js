6bee82e185763a94d23f37a8186984a5ead9d694
umeme522 <yutaro.umehara@gmail.com>
2026-05-06 01:03:43 +0900

    setup: add deployment workflow and update base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/AppPortal/%E7%B5%84%E7%B9%94%E5%9B%B3%E3%82%A2%E3%83%97%E3%83%AA/',
+    base: '/org-app/',
 })
